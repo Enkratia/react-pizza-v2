13bbb22de05ebb2b3adb3fe5d37ca67dc308efa6
Enkratia <manofthetreasure@gmail.com>
2023-06-28 23:52:11 +0300

    m

---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = () => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={466}
+    viewBox="0 0 280 466"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+    <rect x="0" y="265" rx="8" ry="8" width="280" height="27" />
+    <circle cx="140" cy="120" r="120" />
+    <rect x="0" y="315" rx="8" ry="8" width="280" height="88" />
+    <rect x="0" y="440" rx="8" ry="8" width="71" height="26" />
+    <rect x="128" y="421" rx="30" ry="30" width="152" height="45" />
+  </ContentLoader>
+);
+
+export default Skeleton;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+export const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://649b1b1abf7c145d023a03a8.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
